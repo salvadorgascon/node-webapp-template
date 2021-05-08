d8a6488cb4c67d1ca904f590c5daee3a17e24b01
Salvador Gascon <66743758+salvadorgascon@users.noreply.github.com>
2021-05-08 20:07:58 +0200

    Fix metronic plugins

--- src/assets/scripts/vendors/plugins/index.js
+++ src/assets/scripts/vendors/plugins/index.js
@@ -13,35 +13,4 @@ window.bootbox = bootbox;
 import 'bootbox/dist/bootbox.locales.min';
 
 // Typeahead.js - a flexible JavaScript library that provides a strong foundation for building robust typeaheads: https://twitter.github.io/typeahead.js/
-// window.Bloodhound = require('typeahead.js/dist/typeahead.bundle');
-
-// Sticky-js - A library for sticky elements written in vanilla javascript. Learn more: https://rgalus.github.io/sticky-js/
-// window.Sticky = require('sticky-js');
-
-// Perfect-Scrollbar - Minimalistic but perfect custom scrollbar plugin.  Learn more:  https://github.com/mdbootstrap/perfect-scrollbar
-// window.PerfectScrollbar = require('perfect-scrollbar/dist/perfect-scrollbar');
-
-// FormValidation - Best premium validation library for JavaScript. Zero dependencies. Learn more: https://formvalidation.io/
-// window.FormValidation = require("@/src/plugins/formvalidation/dist/js/FormValidation.full.min.js");
-// window.FormValidation.plugins.Bootstrap = require("@/src/plugins/formvalidation/dist/amd/plugins/Bootstrap.js").default;
-
-// JQuery Mask Plugin - is a jQuery plugin which create an input mask. An input mask helps the user with the input by ensuring a predefined format: https://igorescobar.github.io/jQuery-Mask-Plugin/
-// require('jquery-mask-plugin');
-
-// Bootstrap Datepicker - Bootstrap-datepicker provides a flexible datepicker widget in the Bootstrap style: https://bootstrap-datepicker.readthedocs.io/en/latest/
-// require('bootstrap-datepicker/dist/js/bootstrap-datepicker.js');
-// require('@/src/js/vendors/plugins/bootstrap-datepicker.init.js');
-
-// Bootstrap Timepicker - Easily select a time for a text input using your mouse or keyboards arrow keys: https://jdewit.github.io/bootstrap-timepicker/
-// require('bootstrap-timepicker/js/bootstrap-timepicker.js');
-// require('@/src/js/vendors/plugins/bootstrap-timepicker.init.js');
-
-// Bootstrap Touchspin - A mobile and touch friendly input spinner component for Bootstrap 3: https://www.virtuosoft.eu/code/bootstrap-touchspin/
-// require('bootstrap-touchspin/dist/jquery.bootstrap-touchspin.js');
-
-// Bootstrap Switch - Bootstrap switch/toggle is a simple component used for activating one of two predefined options: https://mdbootstrap.com/docs/jquery/forms/switch/
-// require('bootstrap-switch/dist/js/bootstrap-switch.js');
-// require('@/src/js/vendors/plugins/bootstrap-switch.init.js');
-
-// Inputmask - is a javascript library which creates an input mask: https://github.com/RobinHerbots/Inputmask
-// require('inputmask/dist/jquery.inputmask.js');
+window.Bloodhound = require('typeahead.js/dist/typeahead.bundle');
--- src/assets/scripts/vendors/plugins/metronic.js
+++ src/assets/scripts/vendors/plugins/metronic.js
@@ -45,21 +45,21 @@ require('bootstrap-daterangepicker/daterangepicker.js');
 
 // Bootstrap Maxlength - This plugin integrates by default with Twitter bootstrap using badges to display the maximum length of the field where the user is inserting text: https://github.com/mimo84/bootstrap-maxlength
 require('bootstrap-maxlength/src/bootstrap-maxlength.js');
-// require('@/src/plugins/bootstrap-multiselectsplitter/bootstrap-multiselectsplitter.min.js');
+require('scripts/vendors/plugins/metronic/bootstrap-multiselectsplitter/bootstrap-multiselectsplitter.min.js');
 
 // Select2 - Select2 is a jQuery based replacement for select boxes: https://select2.org/
 require('select2/dist/js/select2.full.min.js');
-// require('@/src/js/vendors/plugins/select2.init.js');
+require('scripts/vendors/includes/metronic/vendors/plugins/select2.init.js');
 
 // Flatpickr - is a lightweight and powerful datetime picker.
 require('flatpickr/dist/flatpickr.min.js');
-// require('@/src/js/vendors/plugins/flatpickr.init.js');
+require('scripts/vendors/includes/metronic/vendors/plugins/flatpickr.init.js');
 
 // Inputmask - is a javascript library which creates an input mask: https://github.com/RobinHerbots/Inputmask
 require('inputmask/dist/jquery.inputmask.min.js');
 
 // noUiSlider - is a lightweight range slider with multi-touch support and a ton of features. It supports non-linear ranges, requires no external dependencies: https://refreshless.com/nouislider/
-// window.noUiSlider = require('nouislider/distribute/nouislider.min.js');
+window.noUiSlider = require('nouislider/dist/nouislider.min.js');
 
 // The autosize - function accepts a single textarea element, or an array or array-like object (such as a NodeList or jQuery collection) of textarea elements: https://www.jacklmoore.com/autosize/
 window.autosize = require('autosize/dist/autosize.min.js');
@@ -69,33 +69,33 @@ window.ClipboardJS = require('clipboard/dist/clipboard.min.js');
 
 // DropzoneJS -  is an open source library that provides drag'n'drop file uploads with image previews: https://www.dropzonejs.com/
 window.Dropzone = require('dropzone/dist/min/dropzone.min.js').default;
-// require('@/src/js/vendors/plugins/dropzone.init.js');
+require('scripts/vendors/includes/metronic/vendors/plugins/dropzone.init.js');
 
 // Quill - is a free, open source WYSIWYG editor built for the modern web. Completely customize it for any need with its modular architecture and expressive API: https://quilljs.com/
 // window.Quill = require('quill/dist/quill.js');
 
 // Tagify - Transforms an input field or a textarea into a Tags component, in an easy, customizable way, with great performance and small code footprint, exploded with features: https://github.com/yairEO/tagify
-// require('@yaireo/tagify/dist/tagify.polyfills.min.js');
-// window.Tagify = require('@yaireo/tagify/dist/tagify.min.js');
+require('@yaireo/tagify/dist/tagify.polyfills.min.js');
+window.Tagify = require('@yaireo/tagify/dist/tagify.min.js');
 
 // Toastr - is a Javascript library for non-blocking notifications. jQuery is required. The goal is to create a simple core library that can be customized and extended: https://github.com/CodeSeven/toastr
 window.toastr = require('toastr/build/toastr.min.js');
 
 // Bootstrap Session Timeout - Session timeout and keep-alive control with a nice Bootstrap warning dialog: https://github.com/orangehill/bootstrap-session-timeout
-// window.sessionTimeout = require('@/src/plugins/bootstrap-session-timeout/dist/bootstrap-session-timeout.min.js');
+window.sessionTimeout = require('scripts/vendors/plugins/metronic/bootstrap-session-timeout/dist/bootstrap-session-timeout.min.js');
 
 // JQuery Idletimer - provides you a way to monitor user activity with a page: https://github.com/thorst/jquery-idletimer
-// require('@/src/plugins/jquery-idletimer/idle-timer.min.js');
+require('scripts/vendors/plugins/metronic/jquery-idletimer/idle-timer.min.js');
 
 // ES6 Promise Polyfill - This is a polyfill of the ES6 Promise: https://github.com/lahmatiy/es6-promise-polyfill
 require('es6-promise-polyfill/promise.min.js');
 
 // Sweetalert2 - a beautiful, responsive, customizable and accessible (WAI-ARIA) replacement for JavaScript's popup boxes: https://sweetalert2.github.io/
-// window.Swal = window.swal = require('sweetalert2/dist/sweetalert2.min.js');
+window.Swal = window.swal = require('sweetalert2/dist/sweetalert2.min.js');
 // require('@/src/js/vendors/plugins/sweetalert2.init.js');
 
 // CountUp.js - is a dependency-free, lightweight JavaScript class that can be used to quickly create animations that display numerical data in a more interesting way.
-// window.countUp = require('countup.js/dist/countUp.withPolyfill.min.js');
+window.countUp = require('countup.js/dist/countUp.withPolyfill.min.js');
 
 // Chart.js - Simple yet flexible JavaScript charting for designers & developers
 // window.Chart = require('chart.js/dist/Chart.js');
@@ -104,4 +104,4 @@ require('es6-promise-polyfill/promise.min.js');
 // window.tns = require('tiny-slider/src/tiny-slider.js').tns;
 
 // A lightweight script to animate scrolling to anchor links
-// window.SmoothScroll = require('smooth-scroll/dist/smooth-scroll.js');
+window.SmoothScroll = require('smooth-scroll/dist/smooth-scroll.js');
