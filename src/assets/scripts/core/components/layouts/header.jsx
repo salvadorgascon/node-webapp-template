46f51a606f9a6889d854718cf6cf31a7c5e4b479
Salvador Gascon <66743758+salvadorgascon@users.noreply.github.com>
2021-05-10 01:35:54 +0200

    Fix links

--- src/assets/scripts/core/components/layouts/header.jsx
+++ src/assets/scripts/core/components/layouts/header.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 
 import PropTypes from 'prop-types'
 
+import { Link } from "react-router-dom";
+
 import Logo from 'media/metronic/logos/logo-2-dark.svg'
 
 import Navbar from 'scripts/core/components/layouts/navbar'
@@ -13,7 +15,7 @@ const Header = (props)=> {
 	<div id="kt_header" className="header align-items-stretch">
 		<div className="container-fluid d-flex align-items-stretch justify-content-between">
 			<div className="d-flex align-items-center flex-grow-1 flex-lg-grow-0 me-lg-15">		
-				<a href="index.html"><img alt="Logo" src={Logo} className="h-35px"/></a>		
+				<Link to="/"><img alt="Logo" src={Logo} className="h-35px"/></Link>		
 			</div>
 			<div className="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
 				<Navbar/>
