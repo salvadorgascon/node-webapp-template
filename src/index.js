8829cc202b58ecbc49c5b1b0acfd3b285f88c301
Salvador Gascon <66743758+salvadorgascon@users.noreply.github.com>
2021-05-09 02:25:30 +0200

    Fix static for express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const express = require('express');
 
+const port = 8000;
+
 const app = express();
 
-app.use(express.static("../build"));
+app.use(express.static(path.join(__dirname,'../build')));
 
-app.listen(8000, () => {
-    console.log(`App listening to 8000....`)
+app.listen(port, () => {
+    console.log(`App listening to ${port}....`)
     console.log('Press Ctrl+C to quit.')
 });
