71f93a069f0a0d4fa4b399f6500632a518d4d20e
Salvador Gascon <66743758+salvadorgascon@users.noreply.github.com>
2021-05-09 23:55:32 +0200

    Improve toolbar

--- src/assets/scripts/core/components/layouts/toolbar.jsx
+++ src/assets/scripts/core/components/layouts/toolbar.jsx
@@ -16,18 +16,17 @@ const Toolbar = (props)=> {
 	return (
     <div className="toolbar" id="kt_toolbar">
         <div id="kt_toolbar_container" className="container-fluid d-flex flex-stack">
-
-        <div data-kt-place="true" data-kt-place-mode="prepend" data-kt-place-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" className="page-title d-flex align-items-center me-3 flex-wrap mb-5 mb-lg-0 lh-1">									
-            <PageTitle text="Home"/>
-            <PageDescription text="aassa"/>
-            <TextSeparator/>
-            <Breadcrumb>
-                <BreadcrumbHome/>
-                <BreadcrumbRoute text="Customers" route="/customers"/>
-            </Breadcrumb>
-        </div>
-        <div className="d-flex align-items-center py-1">
-        </div>
+            <div data-kt-place="true" data-kt-place-mode="prepend" data-kt-place-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" className="page-title d-flex align-items-center me-3 flex-wrap mb-5 mb-lg-0 lh-1">									
+                <PageTitle text="Home"/>
+                <PageDescription text="aassa"/>
+                <TextSeparator/>
+                <Breadcrumb>
+                    <BreadcrumbHome/>
+                    <BreadcrumbRoute text="Customers" route="/customers"/>
+                </Breadcrumb>
+            </div>
+            <div className="d-flex align-items-center py-1">
+            </div>
         </div>
     </div>);
 }
