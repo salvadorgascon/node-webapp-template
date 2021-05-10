0da82bac568c855c8b6535746ded2fad877419e0
Salvador Gascon <66743758+salvadorgascon@users.noreply.github.com>
2021-05-11 00:27:45 +0200

    Add separator and fix classnames

---
+++ src/assets/scripts/core/components/labels/block-separator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import PropTypes from 'prop-types'
+
+const BlockSeparator = (props)=> {
+	return (<div className="separator my-10"></div>)
+}
+
+export default BlockSeparator
--- src/assets/scripts/core/components/labels/bullet.jsx
+++ src/assets/scripts/core/components/labels/bullet.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Bullet = ()=> {
-	return (<span class="bullet bullet-dot"></span>)
+	return (<span className="bullet bullet-dot"></span>)
 }
 
 export default Bullet
